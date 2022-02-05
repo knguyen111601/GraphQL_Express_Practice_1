8a2fc18e868678cd6d9079b6bea8f16aece619b2
knguyen111601 <knguyen111601@gmail.com>
2022-02-04 19:25:04 -0800

    Created graphql schema with object and fields as well as queried it

--- server.js
+++ server.js
@@ -1,4 +1,31 @@
 const express = require("express") // import express
+const {graphqlHTTP} = require('express-graphql')
+const {
+    GraphQLSchema,
+    GraphQLObjectType, // lets you write dynamic types
+    GraphQLString
+} = require("graphql")
 const app = express() 
 
-app.listen(5000, ()=>{console.log("Server running on PORT 5000")})
+// Schema 
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({ // Creating object with name HelloWorld that contains messages that are type strings
+        name: "HelloWorld", 
+        fields: ()=> ({ // fields of the object to query
+            message: {
+                type: GraphQLString, // Make the resolve must return a string
+                resolve: (parent, args) => "Hello World" // What actual information the query should return
+            }
+        })
+    })
+})
+
+// Routes/Router
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}))
+
+// App Listener
+app.listen(5000, () => console.log('Server Running'))
